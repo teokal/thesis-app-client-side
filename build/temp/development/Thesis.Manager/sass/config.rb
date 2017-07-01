bc508c7457e5fc472d6d2eb60cc808d3dcf90e44
Thodoris Kalatzis <teo.kal@hotmail.com>
2017-07-01 12:40:05 +0300

    Revert "Add .gitignore"
    This reverts commit acd958364f53033f88f3f29de727442af6248c22.

---
+++ build/temp/development/Thesis.Manager/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\Users\n.vasileiadis\Documents\nikos\Thesis\thesis-app-client-side/build/.sass-cache'
---
+++ build/temp/development/Thesis.Manager/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\n.vasileiadis\Documents\nikos\Thesis\thesis-app-client-side\app.js
