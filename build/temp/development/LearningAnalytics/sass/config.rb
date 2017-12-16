f1a12543e9b20dcd174808339533994030f82dd6
Thodoris Kalatzis <teo.kal@hotmail.com>
2017-12-16 18:17:10 +0200

    Add gitignore

--- build/temp/development/LearningAnalytics/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/Users/n.vasileiadis/Thesis/thesis-app-client-side/build/.sass-cache'
--- build/temp/production/LearningAnalytics/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/n.vasileiadis/Thesis/thesis-app-client-side/app.js
